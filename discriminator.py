e0fa91136af98b174ed17424eb72304ba89d8654
idealwhite <LisaiZhang@foxmail.com>
2019-09-04 20:20:30 +0800

    change D, add a conv layer

--- discriminator.py
+++ discriminator.py
@@ -5,7 +5,7 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        self.conv = nn.Sequential(nn.Conv2d(3, 32, kernel_size=4, stride=2),
+        self.conv = nn.Sequential(nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1),
                                   nn.BatchNorm2d(32),
                                   nn.LeakyReLU(),
 
@@ -13,18 +13,20 @@ class Discriminator(nn.Module):
                                   nn.BatchNorm2d(64),
                                   nn.LeakyReLU(),
 
-                                  nn.Conv2d(64, 128, kernel_size=4, stride=2),
+                                  nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
                                   nn.BatchNorm2d(128),
                                   nn.LeakyReLU(),
 
-                                  nn.Conv2d(128, 256, kernel_size=4, stride=2)
+                                  nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+                                  nn.BatchNorm2d(256),
+                                  nn.LeakyReLU(),
+
+                                  nn.Conv2d(256, 1, kernel_size=4, stride=1)
                                   )
-        self.dense = nn.Linear(1024, 1)
 
     def forward_froze(self, image):
         features = self.conv(image)
-        features = features.view(len(image), -1)
-        logit = self.dense(features)
+        logit = features.view(len(image), -1)
         return logit
 
     def forward_update(self, batch_image, batch_fake):
